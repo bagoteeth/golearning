077329caac54c20e87aa47e4dca5ad59f690f1f4
bagoteeth <1910610016@qq.com>
2021-12-03 14:50:23 +0800

    bb

--- testChan2.go
+++ testChan2.go
@@ -1,16 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"sync"
+)
 
 var mychan = make(chan int, 500)
+var done2 = make(chan struct{})
+var wg = sync.WaitGroup{}
 
 func main() {
+
+	wg.Add(2)
 	go dosth()
 	go func() {
-		for i := 1; i <= 1000; i++ {
+		for i := 1; i <= 10000; i++ {
 			mychan <- i
 		}
+		close(done2)
+		wg.Done()
 	}()
+	wg.Wait()
 }
 
 func dosth() {
@@ -18,7 +29,13 @@ func dosth() {
 		fmt.Println(len(mychan))
 		select {
 		case a := <-mychan:
-			fmt.Println(a)
+			//fmt.Println(a)
+			a++
+		case <-done2:
+			goto label
 		}
+
 	}
+label:
+	wg.Done()
 }
